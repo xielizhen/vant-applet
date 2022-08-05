632a161d9a144d8637beaa7b290341e13908a150
xielizhen <xielz@hrtps.com>
2022-08-05 13:49:24 +0800

    feat: add storybook

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,18 +3,18 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from './Button';
 
 export default {
-    title: 'ReactComponentLibrary/Button',
+    title: 'Basic/Button',
     component: Button,
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const HelloWorld = Template.bind({});
-HelloWorld.args = {
-    label: 'Hello world!'
-}
-
 export const ClickMe = Template.bind({});
 ClickMe.args = {
     label: 'Click me!'
+}
+
+export const HelloWorld = Template.bind({});
+HelloWorld.args = {
+    label: 'Hello world!'
 }
